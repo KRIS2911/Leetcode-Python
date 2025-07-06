8ae7e2da00f7af9858bdb278ead253213e5ec42c
KRIS2911 <krisai2911@gmail.com>
2025-07-06 12:29:14 +0530

    Leetcode 14 in Python

---
+++ 01_Easy Question/L_14op_longest_comm_pref.py
@@ -0,0 +1,66 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix=""
+        for i in strs:
+            if(len(i)<len(prefix) or prefix == ""):
+                prefix=i
+        need=len(strs)
+        count=0
+        result=""
+        while(need != count):
+            count=0
+            for i in strs:
+                if(prefix == i[:len(prefix)]):
+                    count+=1
+                result=prefix
+                prefix=prefix[:-1]
+        if(need == count):
+            return result
+        else:
+            return ""
+s=Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from typing import List
+
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if not strs:
+#             return ""
+#         prefix = strs[0]
+#         for s in strs[1:]:
+#             while not s.startswith(prefix):
+#                 prefix = prefix[:-1]
+#                 if not prefix:
+#                     return ""
+#         return prefix
+
+# s = Solution()
+# print(s.longestCommonPrefix(["flower", "flow", "flight"]))  # Output: "fl"
+            
